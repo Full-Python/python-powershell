3ddebe82051cdc19ac2756600e4675ac3597e8fa
Irving Leonard <irvingleonard@gmail.com>
2026-05-07 15:56:02 -0400

    Mostly completed base code

--- powershell/__init__.py
+++ powershell/__init__.py
@@ -8,54 +8,54 @@ from subprocess import run as subprocess_run
 
 
 LOGGER = getLogger('__name__')
+TYPE_MAP = {
+	'System.Boolean': bool,
+	'System.Collections.Hashtable': dict,
+	'System.Double': float,
+	'System.Int32': int,
+	'System.Management.Automation.SwitchParameter': None,
+	'System.String': str,
+	'System.String[]': list,
+}
 
 
-class Modules(dict):
-	"""
-
-	"""
-
-	def __init__(self, runner=None, runner_kwargs=None):
-		"""
-
-		:param shell:
-		"""
-
-		super().__init__()
-		self._shell = PowerShell(runner=runner)
-
-	def __missing__(self, name):
-		"""
-
-		:param name:
-		:return:
-		"""
-
-	def keys(self):
-		"""
-
-		:return:
-		"""
-
-		return self._shell('Get-Module')
-
-
-class PipedCommand:
+class PipedCommand(list):
 	"""
 	Describes a piped command chain for PowerShell.
 	"""
 
-	def __init__(self, initial_command_line=None):
+	def __call__(self, runner, /, **kwargs):
+		"""
+
+		"""
+
+		result = runner(str(self), **kwargs)
+		if self._output_is_object:
+			return json_loads(result.stdout)
+		else:
+			return result.stdout
+
+	def __init__(self, initial_command_line=None, input_data=None, output_is_object=True):
 		"""Initialization
 		Add the initial/unique command line to the list of commands in the pipe list
 
 		:param initial_command_line: The initial command line
 		:type initial_command_line: str
+		:param input_data: Start by creating feeding the provided data to the first command
+		:type input_data: str|object
+		:param output_is_object: Triggers a conversion from JSON
+		:type output_is_object: bool
 		"""
 
-		self._commands = []
+		super().__init__()
+		if input_data is not None:
+			if isinstance(input_data, str):
+				self.append(f"'{input_data}'")
+			else:
+				self.extend([f"'{json_dumps(input_data)}'", 'ConvertFrom-Json'])
 		if initial_command_line is not None:
-			self._commands.append(initial_command_line)
+			self.append(initial_command_line)
+		self._output_is_object = output_is_object
 
 	def __or__(self, next_command):
 		"""Add a command to the pipe list
@@ -67,7 +67,7 @@ class PipedCommand:
 		:rtype: PowerShellCommand
 		"""
 
-		self._commands.append(next_command)
+		self.append(next_command)
 		return self
 
 	def __str__(self):
@@ -76,7 +76,8 @@ class PipedCommand:
 		:return:
 		"""
 
-		return ' | '.join(self._commands)
+		extra_command = ['ConvertTo-Json'] if self._output_is_object else []
+		return ' | '.join(self + extra_command)
 
 	@staticmethod
 	def build_command_line(command, /, **kwargs):
@@ -96,6 +97,26 @@ class PipedCommand:
 
 		return ' '.join(result)
 
+	@staticmethod
+	def build_command_line_v2(command, param_def={}, /, **param_values):
+		"""
+
+		:param command:
+		:param param_def:
+		:param param_values:
+		:return:
+		"""
+
+		result = [command]
+		for key, type_ in param_def.items():
+			if key in param_values:
+				if type_ is None:
+					result.append(f'-{key}')
+				else:
+					result.append(f'-{key} {param_values.pop(key)}')
+
+		return ' '.join(result)
+
 	@classmethod
 	def from_components(cls, command, /, **kwargs):
 		"""
@@ -108,41 +129,288 @@ class PipedCommand:
 		return cls(cls.build_command_line(command, **kwargs))
 
 
+class Command(dict):
+	"""
+
+	"""
+
+	_already_loaded = False
+
+	def __call__(self, input_data=None, /, *args, **kwargs):
+		"""
+
+		"""
+
+		cmd = f'{self._module}\\{self._name}'
+		cmd = PipedCommand.build_command_line_v2(cmd, self, **kwargs)
+		cmd = PipedCommand(cmd, input_data=input_data)
+		return cmd(self._runner)
+
+	def __init__(self, module, name, runner):
+		"""
+
+		:param module:
+		:type module:
+		:param name:
+		:type name:
+		:param runner:
+		:type runner:
+		"""
+
+		super().__init__()
+		self._module = module
+		self._name = name
+		self._runner = runner
+
+	def __iter__(self):
+		"""
+
+		"""
+
+		self._load()
+		return super().__iter__()
+
+	def __getitem__(self, item):
+		"""
+		
+		"""
+		
+		self._load()
+		return super().__getitem__(item)
+
+	def __len__(self):
+		"""
+
+		"""
+
+		self._load()
+		return super().__len__()
+
+	def __missing__(self, name):
+		"""
+
+		:param name:
+		:return:
+		"""
+
+		if self._already_loaded:
+			raise KeyError(name)
+
+		self._load()
+		return self[name]
+
+	def _load(self):
+		"""
+
+		"""
+
+		if self._already_loaded:
+			return False
+
+		cmd = PipedCommand(f'Get-Command -Module {self._module} -Name {self._name}', output_is_object=True)
+		cmd |= 'Select-Object -ExpandProperty Parameters'
+		for name, result in cmd(self._runner).items():
+			# self[name] = result['ParameterType']['FullName']
+			self[name] = TYPE_MAP.get(result['ParameterType']['FullName'], dict)
+
+		self._already_loaded = True
+		return True
+
+	def items(self, *args, **kwargs):
+		"""
+
+		"""
+
+		self._load()
+		return super().items(*args, **kwargs)
+
+	def keys(self):
+		"""
+
+		:return:
+		"""
+
+		self._load()
+		return super().keys()
+
+	def values(self):
+		"""
+
+		"""
+
+		self._load()
+		return super().values()
+
+
+class Module(dict):
+	"""
+
+	"""
+
+	_already_loaded = False
+
+	def __init__(self, name, runner):
+		"""
+
+		:param name:
+		:type name:
+		:param runner:
+		:type runner:
+		"""
+
+		super().__init__()
+		self._name = name
+		self._runner = runner
+
+	def __missing__(self, name):
+		"""
+
+		:param name:
+		:return:
+		"""
+
+		if self._already_loaded:
+			raise KeyError(name)
+
+		self._load()
+		return self[name]
+
+	def _load(self):
+		"""
+
+		"""
+
+		if self._already_loaded:
+			return False
+
+		cmd = PipedCommand(f'Get-Command -Module {self._name}', output_is_object=True)
+		for result in cmd(self._runner):
+			self[result['Name']] = Command(self._name, result['Name'], self._runner)
+
+		self._already_loaded = True
+		return True
+
+	def items(self, *args, **kwargs):
+		"""
+
+		"""
+
+		self._load()
+		return super().items(*args, **kwargs)
+
+	def keys(self):
+		"""
+
+		:return:
+		"""
+
+		self._load()
+		return super().keys()
+
+	def values(self):
+		"""
+
+		"""
+
+		self._load()
+		return super().values()
+
+
+class Modules(dict):
+	"""
+
+	"""
+
+	_already_loaded = False
+
+	def __init__(self, runner):
+		"""
+
+		:param shell:
+		"""
+
+		super().__init__()
+		self._runner = runner
+
+	def __missing__(self, name):
+		"""
+
+		:param name:
+		:return:
+		"""
+
+		if self._already_loaded:
+			raise KeyError(name)
+
+		self._load()
+		return self[name]
+
+	def _load(self):
+		"""
+
+		"""
+
+		if self._already_loaded:
+			return False
+
+		cmd = PipedCommand('Get-Module -ListAvailable', output_is_object=True)
+		for result in cmd(self._runner):
+			self[result['Name']] = Module(result['Name'], self._runner)
+
+		self._already_loaded = True
+		return True
+
+	def items(self, *args, **kwargs):
+		"""
+
+		"""
+
+		self._load()
+		return super().items(*args, **kwargs)
+
+	def keys(self):
+		"""
+
+		:return:
+		"""
+
+		self._load()
+		return super().keys()
+
+	def values(self):
+		"""
+
+		"""
+
+		self._load()
+		return super().values()
+
+
 class PowerShell:
 	"""
 
 	"""
 
-	def __call__(self, command_string, input_data=None):
+	def __getattr__(self, name):
 		"""
 
-		:param command_string:
-		:return:
 		"""
 
-		cmd = PipedCommand()
-		if input_data is not None:
-			cmd |= f"'{json_dumps(input_data)}'"
-			cmd |= 'ConvertFrom-Json'
-		cmd |= command_string
-		cmd |= 'ConvertTo-Json'
-		result = self._runner(str(cmd), **self._runner_kwargs)
-		return vars(result)
-		return json_loads(result)
+		if name == 'modules':
+			value = Modules(runner=self._runner)
+		else:
+			raise AttributeError(name)
 
-	def __new__(cls, runner=None, runner_kwargs=None):
+		self.__setattr__(name, value)
+		return value
+
+	def __init__(self, runner=None):
 		"""
 
 		:param runner:
 		"""
 
-		instance = super().__new__(cls)
-		if runner is None:
-			instance._runner = cls.with_subprocess
-		else:
-			instance._runner = runner
-		instance._runner_kwargs = {} if runner_kwargs is None else runner_kwargs
-		return instance
+		self._runner = self.with_subprocess if runner is None else runner
 
 	@staticmethod
 	def with_subprocess(command_string, /, **kwargs):
@@ -151,5 +419,10 @@ class PowerShell:
 		:param command_string:
 		:return:
 		"""
+
+		if 'capture_output' not in kwargs:
+			kwargs['capture_output'] = True
+		if 'text' not in kwargs:
+			kwargs['text'] = True
 
 		return subprocess_run(('powershell', '-Command', command_string), **kwargs)
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-from powershell import Modules
+from powershell import PowerShell, PipedCommand, Modules
+from pprint import pprint as pprint_
 
-mod = Modules(runner_kwargs={'capture_output': True, 'text': True})
-result = mod.keys()
-print('Result is', result, sep=': ')
+ps = PowerShell()
+# mod = Modules(runner_kwargs={'capture_output': True, 'text': True})
+# result = ps.modules['Appx'].keys()
+result = ps.modules['Appx']['Get-AppxPackage']()
+# result = ps.modules['Appx']._load()
+# cmd = PipedCommand('Get-Module -ListAvailable', output_is_object=True)
+# result = cmd(PowerShell.with_subprocess, capture_output=True, text=True)
+# print('Result is', result, sep=': ')
+pprint_(result)
