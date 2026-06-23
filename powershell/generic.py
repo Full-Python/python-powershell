d89ed6fd900bfbd61b0b8047ae759c09b5ad7c1f
Irving Leonard <irvingleonard@gmail.com>
2026-06-23 16:08:07 -0400

    Working on FullKeysDict

--- powershell/generic.py
+++ powershell/generic.py
@@ -63,3 +63,71 @@ class FullLoadDict(dict):
 		"""
 
 		return [self[key] for key in self.keys()]
+
+
+class FullKeysDict(dict):
+	"""Full keys dict
+	A dynamic dict that loads all the keys with a single operation and values with individual ones.
+	"""
+
+	_already_loaded = False
+
+	def __len__(self):
+		"""Dict __len__
+		Override of the builtin dict __len__ method
+		"""
+
+		return len(self.keys())
+
+	@abstractmethod
+	def _load_keys(self):
+		"""Load the dict keys
+		Load all the keys in the dict.
+
+		:return: the keys
+		:rtype: list
+		"""
+
+	@abstractmethod
+	def _load_value(self, name):
+		"""Load the dict
+		Execute the potentially expensive operation to load the dict content.
+
+		:param name: the key for the requested value
+		:type name: Hashable
+		:return: the value matching the key
+		:rtype: Any
+		"""
+
+	def items(self, *args, **kwargs):
+		"""Dict items
+		Override of the builtin dict items method
+
+		:return: a list with the items
+		:rtype: list
+		"""
+
+		return [(key, self[key]) for key in self.keys()]
+
+	def keys(self):
+		"""Dict keys
+		Override of the builtin dict keys method
+
+		:return: a set with the keys
+		:rtype: set
+		"""
+
+		if not self._already_loaded:
+			self.update(self._load_keys())
+
+		return super().keys()
+
+	def values(self):
+		"""Dict values
+		Override of the builtin dict values method
+
+		:return: a list with the values
+		:rtype: list
+		"""
+
+		return [self[key] for key in self.keys()]
